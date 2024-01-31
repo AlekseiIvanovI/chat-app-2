43f51d0b842b25b0f609a26acb125101e689f8b0
Aleksei Ivanov <aleksei.ivanov.qa@gmail.com>
2024-01-30 17:08:17 -0800

    new commit

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,3 +1,23 @@
+'use client'
+
+import clsx from 'clsx'
+import {
+    FieldErrors,
+    FieldValues,
+    UseFormRegister
+} from 'react-hook-form';
+
+interface inputProps{
+    label: string;
+    id: string;
+    type?: string;
+    required?: boolean;
+    register: UseFormRegister<FieldValues>,
+    errors: FieldErrors,
+    disabled?: boolean;
+    
+}
+
 const Input = () => {
   return (
     <div>
