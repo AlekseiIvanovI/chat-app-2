1a87d2e3f5e68102eacee8ee84d8bcb5f58ca7f2
Aleksei Ivanov <aleksei.ivanov.qa@gmail.com>
2024-01-30 18:23:21 -0800

    new commit

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import Input from "@/app/components/inputs/Input";
 import { useCallback, useState } from "react"
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import Button from "../../components/Button";
+import Input from "@/app/components/inputs/Input";
 
 type Variant = 'LOGIN' | "REGISTER"
 
@@ -49,29 +50,78 @@ const AuthForm = () => {
 
   return (
     <div
-    className="
-        mt-8
-        sm:mx-auto
-        sm:w-full
-        sm:max-w-md
-    "
+    className="mt-8 sm:mx-auto sm:w-full sm:max-w-md"
     >
       <div
       className="
-         bg-white
-         px-4
-         py-8
-         shadow
-         sm:rounded-lg
-         sm:px-10
+      bg-white
+      px-4
+      py-8
+      shadow
+      sm:rounded-lg
+      sm:px-10
       "
       >
         <form
-            className="space-white-6"
+            className="space-y-6"
             onSubmit={handleSubmit(onSubmit)}
         >
-            <Input />
+        {variant === "REGISTER" && (
+            <Input 
+            label="Name" 
+            register={register} 
+            id="name"
+            errors={errors}
+            />
+        )}
+        <Input 
+            label="Email" 
+            type="email"
+            register={register} 
+            id="email"
+            errors={errors}
+        />
+        <Input 
+            label="Password" 
+            type="password"
+            register={register} 
+            id="password"
+            errors={errors}
+        />
+        <div>
+            <Button
+                disabled={isLoading}
+                fullWidth
+                type="submit"
+            >
+                {variant === 'LOGIN' ? 'Sign in' : 'Register'}
+            </Button>
+        </div>
         </form>
+        <div className="mt-6">
+            <div className="relative">
+                <div
+                className="
+                absolute
+                inset-0
+                flex
+                items-center
+                "
+                >
+                    <div 
+                        className="
+                        w-full 
+                        border-t 
+                        border-gray-300"
+                        />
+                </div>
+                <div className="relative flex justify-center text-sm">
+                    <span className="bg-white px-2 text-gray-500">
+                        Or continue with
+                    </span>
+                </div>
+            </div>
+        </div>
       </div>
     </div>
   )
