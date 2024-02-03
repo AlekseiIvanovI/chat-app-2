9ed7c15fe6d1aaefa8fa5987b8654d5b3ad06d59
Aleksei Ivanov <aleksei.ivanov.qa@gmail.com>
2024-02-02 23:23:21 -0800

    new commit

--- pages/api/pusher/auth.ts
+++ pages/api/pusher/auth.ts
@@ -2,15 +2,14 @@ import { NextApiRequest, NextApiResponse } from "next"
 import { getServerSession } from "next-auth";
 
 import { pusherServer } from "@/app/libs/pusher";
-import authOptions from "@/app/api/auth/[...nextauth]/route"; // Change import statement
-
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function handler(
   request: NextApiRequest, 
   response: NextApiResponse
 ) {
   const session = await getServerSession(request, response, authOptions);
-// @ts-ignore
+
   if (!session?.user?.email) {
     return response.status(401);
   }
@@ -18,40 +17,9 @@ export default async function handler(
   const socketId = request.body.socket_id;
   const channel = request.body.channel_name;
   const data = {
-    // @ts-ignore
     user_id: session.user.email,
   };
 
   const authResponse = pusherServer.authorizeChannel(socketId, channel, data);
   return response.send(authResponse);
 };
-
-
-
-
-// import { NextApiRequest, NextApiResponse } from "next"
-// import { getServerSession } from "next-auth";
-
-// import { pusherServer } from "@/app/libs/pusher";
-// import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-
-
-// export default async function handler(
-//   request: NextApiRequest, 
-//   response: NextApiResponse
-// ) {
-//   const session = await getServerSession(request, response, authOptions);
-
-//   if (!session?.user?.email) {
-//     return response.status(401);
-//   }
-
-//   const socketId = request.body.socket_id;
-//   const channel = request.body.channel_name;
-//   const data = {
-//     user_id: session.user.email,
-//   };
-
-//   const authResponse = pusherServer.authorizeChannel(socketId, channel, data);
-//   return response.send(authResponse);
-// };
