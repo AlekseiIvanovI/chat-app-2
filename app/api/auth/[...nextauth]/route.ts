dc31446307665b8952d923652c6dda96b291dde6
Aleksei Ivanov <aleksei.ivanov.qa@gmail.com>
2024-02-02 21:54:22 -0800

    new commit

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,10 @@
-import bcrypt from "bcrypt"
-import { AuthOptions } from "next-auth";
+import bcrypt from "bcrypt";
+import NextAuth, { AuthOptions } from "next-auth";
 import prisma from '@/app/libs/prismadb';
-import NextAuth from "next-auth"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
-import CredentialsProvider from "next-auth/providers/credentials"
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 // Define authOptions
 const authOptions: AuthOptions = {
